3d1a07f9a058c451195ec99e849c3b297004a018
eytienne <arabeyre.etienne@gmail.com>
2020-02-16 10:40:44 +0100

    BP cleaned and goBack fixed

--- my_c_lib/BinaryPath.c
+++ my_c_lib/BinaryPath.c
@@ -16,6 +16,7 @@ void __goTo(BinaryPath *bp, enum DIRECTION d) {
 		unsigned char *newBits = realloc(bp->bits, i + 1);
 		assert(newBits != NULL);
 		bp->bits = newBits;
+		bp->bits[i] = 0;
 	}
 	bp->bits[i] = (bp->bits[i]) | (d << bp->length % 8);
 	bp->length++;
@@ -28,25 +29,30 @@ void goToRight(BinaryPath *bp) { __goTo(bp, RIGHT); }
 void goBack(BinaryPath *bp) {
 	assert(bp->length > 0);
 	size_t i = bp->length / 8;
-	printf("\n%lu (%lu)\n", bp->length, i);
 	if (bp->length % 8 == 0) {
 		unsigned char *newBits = realloc(bp->bits, i);
 		assert(newBits != NULL);
 		bp->bits = newBits;
-		printf("realloc!\n");
 	} else {
-		bp->bits[i] &= bp->length % 8;
+		bp->bits[i] &= 255 >> (8 - (bp->length) % 8);
 	}
 	bp->length--;
 }
 
 int getBPStep(const BinaryPath *bp, size_t index) {
 	assert(index >= 0);
+	assert(index < (bp->length / 8 + (bp->length % 8 != 0)) * 8);
 	return ((bp->bits[index / 8] >> (index % 8)) & 1) != 0;
 }
 
 void printBinaryPath(const BinaryPath *bp) {
-	for (size_t i = 0; i < bp->length; i++) {
+	for (size_t i = 0;
+
+		 //  i < bp->length
+		 i < (bp->length / 8 + (bp->length % 8 != 0)) * 8
+
+		 ;
+		 i++) {
 		printf("%hhu", getBPStep(bp, i));
 		if ((i + 1) % 8 == 0)
 			printf(" ");
